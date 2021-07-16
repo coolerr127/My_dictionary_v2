a40c7b507c1d380bb8c81f533892f168a3f1a5d5
Anton Shimanovich <ashimanovich127@gmail.com>
2021-07-16 23:20:58 +0300

    start proj

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+	return (
+		<header className='header'>
+			<div className='header-wrapper'>
+				<a href="/" className="logo">My dictionary</a>
+			</div>
+		</header>
+	)
+}
+
+export default Header
